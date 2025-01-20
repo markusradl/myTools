c0efa202b0137099ab86a18644aec56e03333fb9
Markus Radl <markus@markusradl.com>
2025-01-21 00:06:32 +0100

    Export also Placeholder Info into DB

--- s7tools/zpl2db/label.zpl
+++ s7tools/zpl2db/label.zpl
@@ -3,12 +3,12 @@
 ^FX --- Text 1
 ^FO10,10
 ^A0,40
-^FD${3}^FS
+^FD${0}^FS
 
 ^FX --- Barcode
 ^BY5,2,270
 ^FO50,150
 ^BC
-^FD${3}^FS
+^FD${1}^FS
 
 ^XZ
--- s7tools/zpl2db/zpl2db.ts
+++ s7tools/zpl2db/zpl2db.ts
@@ -1,22 +1,47 @@
-import { parseArgs } from 'jsr:@std/cli/parse-args'
+//import { parseArgs } from 'jsr:@std/cli/parse-args'
 
 async function readAndConvertZPL(filePath: string) {
     const content = await Deno.readTextFile(filePath)
-    const test = content.replace(/\$/g, '$$$$').replace(/\n/g, '$N')
-    console.log(test)
+    const flattenedString = content.replace(/\n/g, '$N') // Siemens strings uses two chars for newline (String Length!!)
+    const templateString = flattenedString.replace(/\$/g, '$$$$') // Siemens strings escapes $ with $$
+
+    // Search for occurrences of ${x} where x is 1 to 9 and write the indices into a new array
+    const regex = /\$\{([0-9])\}/g
+    const matches = [...flattenedString.matchAll(regex)]
+
+    const placeHolders = matches.map((match) => {
+        return {
+            id: match[1],
+            position: match.index,
+        }
+    })
+
+    const placeHoldersString = placeHolders
+        .map((ph, index) => {
+            return `
+            PlaceHolders[${index}].Id := ${ph.id};
+            PlaceHolders[${index}].Position := ${ph.position};`
+        })
+        .join('\n')
+
     const dbsource = `
-        DATA_BLOCK "Label"
+        DATA_BLOCK "LabelData"
         { S7_Optimized_Access := 'TRUE' }
         VERSION : 0.1
         NON_RETAIN
-            VAR 
-                data : WString;
-            END_VAR
+        VAR 
+            TemplateString : WString;
+            PlaceHolders : Array[0..${placeHolders.length - 1}] of Struct
+                Id : Int;   // Placeholder ID (0..9)
+                Position : Int;   // Startposition in template string
+            END_STRUCT;
+        END_VAR
 
         BEGIN
-            data := WSTRING#'${test}';
-        END_DATA_BLOCK
-        `
+            TemplateString := WSTRING#'${templateString}';
+            ${placeHoldersString}
+        END_DATA_BLOCK`
+
     await Deno.writeTextFile('label.db', dbsource)
 }
 
